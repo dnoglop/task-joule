1c91b94071088d521c222446da63d14258802ae1
dnoglop <d169692@dac.unicamp.br>
2025-07-22 15:42:21 -0300

    [dyad] Corrigindo erro de sintaxe na importação do Button - wrote 1 file(s)

--- src/components/EmployeeFormDialog.tsx
+++ src/components/EmployeeFormDialog.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
-import { Button } => '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
